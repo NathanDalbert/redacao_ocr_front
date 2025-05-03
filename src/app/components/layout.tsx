227855b32ccccb6a8770023355430452ba7cefc1
NathanDalbert <dalbert.nathan@gmail.com>
2025-05-03 19:17:35 -0300

    feat: criar componente que define loout da pagina

---
+++ src/app/components/layout.tsx
@@ -0,0 +1,15 @@
+import { Inter } from "next/font/google";
+import React from "react";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Detector de Texto em Redações"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body className={`${inter.className} bg-gray-950 text-white`}>{children}</body>
+    </html>
+  );
+}
